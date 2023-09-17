694e6ff76c999c20b4ff5ef283ec9ea2cb3a0c55
Esther-Wagatwe <estherwagatwe@gmail.com>
2023-09-17 14:04:39 -0700

    created switchcase function

--- printf.c
+++ printf.c
@@ -6,45 +6,27 @@
  */
 int _printf(const char *format, ...)
 {
-int numchar = 0;
-va_list args;
-if (format == NULL)
-return (-1);
-
-va_start(args, format);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case '%':
-{
-write(1, format, 1);
-}
-break;
-case 's':
-{
-char *string = va_arg(args, char *);
-int i = 0;
-while (string[i] != '\0')
-{
-i++;
-}
-write(1, string, i);
-numchar += i;
-}
-break;
-}
-}
-else
-{
-write(1, format, 1);
-numchar++;
-}
-format++;
-}
-va_end(args);
-return (numchar);
+    int numchar = 0;
+    va_list args;
+    if (format == NULL)
+    {
+        return (-1);
+    }
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++; // Move past '%'
+            caseCheck(format, numchar, args);
+        }
+        else
+        {
+            write(1, format, 1);
+            numchar++;
+        }
+        format++;
+    }
+    va_end(args);
+    return numchar;
 }
---
+++ switchcase.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+  * caseCheck - check the format
+  * @format: arguments passed in printf func
+  * @numchar: character count
+  * @args: Parameters
+  * Return: Int
+  */
+int caseCheck(const char *format, int numchar, va_list args)
+{
+	switch (*format)
+	{
+		case '%':
+			{
+				write(1, format, 1);
+				numchar++;
+			}
+			break;
+		case 'c':
+			{
+				int character = va_arg(args, int);
+
+				write(1, &character, 1);
+
+				numchar++;
+			}
+			break;
+		case 's':
+			{
+				char *string = va_arg(args, char *);
+				int i = 0;
+
+				while (string[i] != '\0')
+				{
+					i++;
+				}
+
+				write(1, string, i);
+				numchar += i;
+			}
+			break;
+	}
+}
