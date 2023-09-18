2c309c2bbbca37e2bd3b33788da06b7af59eb56a
Esther-Wagatwe <estherwagatwe@gmail.com>
2023-09-18 22:42:33 +0300

    betty corrections

--- print_rev.c
+++ print_rev.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+  * printf_reverse - function that prints reverse
+  * @args: arguments to be passed
+  * @numchar: number of characters printed
+  * Return: return numchar
+  */
 int printf_reverse(va_list args, int numchar)
 {
 	char *str = va_arg(args, char *);
 	int len = 0, i;
+
 	while (str[len])
 	{
 		len++;
