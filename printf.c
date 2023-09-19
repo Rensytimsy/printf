74f5df6e3ae5f12f528f6fc6c9432c3aa75939fe
Rensytimsy <rensytimsy03@gmail.com>
2023-09-19 17:27:42 +0300

    all func

--- printf.c
+++ printf.c
@@ -1,35 +1,54 @@
 #include "main.h"
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
+ *
+*/
+void print_buffer(char buffer[], int *buff_ind);
+
 int _printf(const char *format, ...)
 {
-	int numchar = 0;
-	va_list args;
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
+if (format == NULL)
+return (-1);
+va_start(list, format);
+for (i = 0; format && format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/* write(1, &format[i], 1);*/
+printed_chars++;
+}
+else
+{
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
+}
+}
+print_buffer(buffer, &buff_ind);
+va_end(list);
+return (printed_chars);
+}
 
-	if (format == NULL)
-	{
-		return (-1);
-	}
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++; /* Move past '%'*/
-			numchar += caseCheck(format, numchar, args);
-		}
-		else
-		{
-		       write(1, format, 1);
-			numchar++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (numchar);
+/**
+ * 
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+*buff_ind = 0;
 }
